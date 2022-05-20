c816e87f833a95a899788f4311ee6484bebe6240
Richter17 <36236219+Richter17@users.noreply.github.com>
2022-05-20 12:40:00 +0300

    added debug

--- repoTest/Assets/MyMono.cs
+++ repoTest/Assets/MyMono.cs
@@ -7,7 +7,7 @@ public class MyMono : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello world!");
     }
 
     // Update is called once per frame
